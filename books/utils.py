b668cb3d6d16f040cf73241242ad3685576ed16c
Samandar587 <samandar0311@gmail.com>
2023-08-13 05:24:12 +0500

    small changes

--- books/utils.py
+++ books/utils.py
@@ -1,6 +1,6 @@
 from .models import Book
 
-def is_book_available(book_id, quantity, cart_id):
+def is_book_available(book_id, quantity):
     try:
         book = Book.objects.get(pk=book_id)
         return book.quantity >= quantity
