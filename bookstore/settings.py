60b89d6d862e866302c658e35dc72b95be1e48ef
Samandar587 <samandar0311@gmail.com>
2023-08-18 21:45:40 +0500

    changes in settings

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 import os, datetime
 
 load_dotenv()
-DB_NAME=os.environ.get("DB_NAME", "bookstore")
-DB_USER=os.environ.get("DB_USER", "postgres")
-DB_PASSWORD=os.environ.get("DB_PASSWORD", "postgres")
-DB_HOST=os.environ.get("DB_HOST", "db")
-DB_PORT=os.environ.get("DB_PORT", 5432)
+DB_NAME=os.environ.get("DB_NAME")
+DB_USER=os.environ.get("DB_USER")
+DB_PASSWORD=os.environ.get("DB_PASSWORD")
+DB_HOST=os.environ.get("DB_HOST")
+DB_PORT=os.environ.get("DB_PORT")
 
 AUTH_USER_MODEL = "books.User"
 
